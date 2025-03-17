2c3ff094186fc95ba844d828e34efbc85084d441
fefo <fefo>
2025-03-16 22:42:54 -0300

    fix: removendo arquivos estáticos inutilizados

--- service_control/models.py
+++ service_control/models.py
@@ -3,18 +3,18 @@ from accounts.models import BaseModel
 from django.contrib.auth.models import User
 
 
-class ServiceOrder(BaseModel):
-    order_date = models.DateField()
-    return_date = models.DateField()
-    suit_measure = models.IntegerField()
-    shirt_measure = models.IntegerField()
-    pants_measure = models.IntegerField()
-    occasion = models.CharField(max_length=255) 
+# class ServiceOrder(BaseModel):
+#     order_date = models.DateField()
+#     return_date = models.DateField()
+#     suit_measure = models.IntegerField()
+#     shirt_measure = models.IntegerField()
+#     pants_measure = models.IntegerField()
+#     occasion = models.CharField(max_length=255) 
 
-    renter = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name="renter_%(class)s")
+#     renter = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name="renter_%(class)s")
 
-    class Meta:
-        db_table = "person"  
+#     class Meta:
+#         db_table = "service_order"  
 
-    def __str__(self):
-        return f"{self.nome} ({self.person_type.nome})" 
+#     def __str__(self):
+#         return f"{self.nome} ({self.person_type.nome})" 
